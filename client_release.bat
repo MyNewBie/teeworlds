b7808529fc3e5d280f22b98defaa09a5bcd71e50
NoxNebula <patrick.kl@live.de>
2011-05-03 04:03:09 +0200

    fixed compiling errors for standard teeworlds

---
+++ client_release.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:: Check for Visual Studio
+if exist "%VS100COMNTOOLS%" (
+ set VSPATH="%VS100COMNTOOLS%..\..\VC\"
+ goto set_c
+)
+if exist "%VS90COMNTOOLS%" (
+ set VSPATH="%VS90COMNTOOLS%..\..\VC\"
+ goto set_c
+)
+if exist "%VS80COMNTOOLS%" (
+ set VSPATH="%VS80COMNTOOLS%..\..\VC\"
+ goto set_c
+)
+
+:set_c
+call %VSPATH%vcvarsall.bat x86
+goto compile
+
+@echo You need Microsoft Visual Studio 8, 9 or 10 installed
+pause
+exit
+
+:compile
+@echo === compiling teeworlds (client release) ===
+@..\bam\bam client
+@echo.
+@pause
