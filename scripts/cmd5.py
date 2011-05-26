c6ced4f27aeab3048c9092480e911651906e04f9
NoxNebula <patrick.kl@live.de>
2011-05-26 20:26:09 +0200

    Forcing 0.6 Nethash - fixes #39

--- scripts/cmd5.py
+++ scripts/cmd5.py
@@ -30,6 +30,7 @@ for filename in sys.argv[1:]:
 
 hash = hashlib.md5(f).hexdigest().lower()[16:]
 #TODO 0.7: improve nethash creation
-if hash == "6e28a475de43adfd":
-	hash = "626fce9a778df4d4"
+#if hash == "6e28a475de43adfd": # Catching: Force Nethash
+#	hash = "626fce9a778df4d4"
+hash = "626fce9a778df4d4"
 print('#define GAME_NETVERSION_HASH "%s"' % hash)
